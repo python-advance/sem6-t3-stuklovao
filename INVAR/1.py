d367c8ff7c5beb75417d52e7db734ddd5cab84d8
stuklovao <36235933+stuklovao@users.noreply.github.com>
2019-06-08 11:47:38 +0300

    Update 1.py

--- INVAR/1.py
+++ INVAR/1.py
@@ -3,6 +3,9 @@ from xml.etree import ElementTree as ET
 import time
 
 def get_currencies(currencies_ids_lst=['R01235', 'R01239', 'R01820']):
+    """
+    получение курсов валют с сайта Центробанка
+    """
     cur_res_str = urlopen("http://www.cbr.ru/scripts/XML_daily.asp")
     result = {}
     cur_res_xml = ET.parse(cur_res_str)
@@ -17,26 +20,41 @@ def get_currencies(currencies_ids_lst=['R01235', 'R01239', 'R01820']):
     return result
 
 class CurrencyBoard():
-
+    """
+    класс-синглтон
+    """
     def __init__(self):
 
         self.currencies = ['R01235','R01239','R01820']
         self.rates = get_currencies(self.currencies)
 
     def get_from_cache(self, code):
+        """
+        без запроса к сайту
+        """
         return self.currencies[code]
 
     def get_new_currency(self, code):
+        """
+        запрос информации о курсах новой валюты  и добавление её в кэш
+        """
         self.currencies.append(code)
         self.rates.update(get_currencies([code]))
         return self.rates[code]
 
     def update(self):
+        """
+        принудительно обновляем данных
+        """
         new = get_currencies(self.currencies)
         self.rates.update(dict(zip(sorted(self.currencies),new.values())))
         return self.rates
 
     def check(self):
+        """
+        проводим проверку времени обновления
+        """
+
         if (time.clock() > 5*60):
             return get_currencies(self.currencies)
         else:
